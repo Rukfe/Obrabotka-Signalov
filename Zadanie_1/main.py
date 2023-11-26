846977b216c30b1df617740bf02d57ae37bf828b
СyberMaid <odinanton111@gmail.com>
2023-11-26 12:48:37 +0300

    123

--- Zadanie_1/main.py
+++ Zadanie_1/main.py
@@ -9,9 +9,15 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 N = 500  # число отсчетов
 M = 65  # порядок фильтра
 M_2 = M // 2
-freqDiskret = 2000 #частота дискретизации
+freqDiskret = 2000  # частота дискретизации
 delta_f = 40  # Ширина полосы частот канала
-freqCenter = [40, 80, 120, 160, 200]  # Центральные частоты каналов (40, 80, 120, 160, 200 Гц)
+freqCenter = [
+    40,
+    80,
+    120,
+    160,
+    200,
+]  # Центральные частоты каналов (40, 80, 120, 160, 200 Гц)
 x = np.zeros(N, dtype=np.float32)  # входной сигнал
 y = np.zeros(N, dtype=np.float32)
 h = np.zeros(M, dtype=np.float32)  # импульсная характеристика
@@ -23,39 +29,43 @@ class Application(tk.Tk):
         self.title("Задание 1 (ЦОС)")
         self.iconbitmap("KFU.ico")
         self.geometry("1200x700")
-        
+
         self.graphIsx = Frame(self)
         self.graphIsx.pack()
         self.graphIsxEmpty()
-        
+
         self.graphSpectr = Frame(self)
         self.graphSpectr.pack()
         self.graphSpectrEmpty()
-        
+
         self.graphCf = Frame(self)
         self.graphCf.pack()
         self.graphCfEmpty()
 
-        self.file_button = tk.Button(self, text="Выбрать файл и построить\nграфик исходного сигнала", command=self.open_file)
+        self.file_button = tk.Button(
+            self,
+            text="Выбрать файл и построить\nграфик исходного сигнала",
+            command=self.open_file,
+        )
         self.file_button.pack(side="left", anchor="sw", ipadx=10, ipady=2)
-        
+
         self.exit_button = tk.Button(self, text="Выход", command=self.quit)
         self.exit_button.pack(side="right", anchor="se", ipadx=10, ipady=10)
-        
+
         self.impBtn = Button(self, text="Спектр", command=self.spectr)
         self.impBtn.pack(side="left", anchor="sw", ipadx=10, ipady=10)
-        
+
         self.cfBtn = Button(self, text="ЦФ", command=partial(self.numChannel))
         self.cfBtn.pack(side="left", anchor="sw", ipadx=10, ipady=10)
-        
+
         self.listBoxIsx = tk.Listbox(self)
         self.listBoxIsx.pack(side="left", anchor="sw", ipadx=5, ipady=2)
-        self.listBoxIsx.insert(tk.END, 'Исходный сигнал')
-        
+        self.listBoxIsx.insert(tk.END, "Исходный сигнал")
+
         self.listBoxCF = tk.Listbox(self)
         self.listBoxCF.pack(side="left", anchor="sw", ipadx=5, ipady=2)
-        self.listBoxCF.insert(tk.END, 'После ЦФ')
-        
+        self.listBoxCF.insert(tk.END, "После ЦФ")
+
     def graphIsxEmpty(self):
         # создание пустого графика
         global isxCanvas
@@ -67,7 +77,7 @@ class Application(tk.Tk):
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
         isxCanvas = self.canvas
-        
+
     def graphSpectrEmpty(self):
         # создание пустого графика
         global spectrCanvas
@@ -78,8 +88,8 @@ class Application(tk.Tk):
         self.canvas = FigureCanvasTkAgg(figImpulse, master=self.graphSpectr)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
-        spectrCanvas = self.canvas 
-        
+        spectrCanvas = self.canvas
+
     def graphCfEmpty(self):
         # создание пустого графика
         global cfCanvas
@@ -90,16 +100,17 @@ class Application(tk.Tk):
         self.canvas = FigureCanvasTkAgg(figCf, master=self.graphCf)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
-        cfCanvas = self.canvas       
-    
+        cfCanvas = self.canvas
+
     def open_file(self):
         global data
-        file_path = filedialog.askopenfilename(filetypes=[('.dat файлы', '*.dat')])
+        file_path = filedialog.askopenfilename(filetypes=[
+            (".dat файлы", "*.dat")])
         if not file_path:
             data = None
             messagebox.showinfo("Внимание!", "Файл не был выбран!")
-            return()
-        with open(file_path, 'rb') as file:
+            return ()
+        with open(file_path, "rb") as file:
             k = 0
             while True:
                 data = file.read(4)  # 4 байта для float32
@@ -108,13 +119,13 @@ class Application(tk.Tk):
                     break
                 x[k] = np.frombuffer(data, dtype=np.float32)[0]
                 y[k] = np.frombuffer(data, dtype=np.float32)[0]
-                self.listBoxIsx.insert(tk.END, f'{k + 1}.  {x[k]}')
+                self.listBoxIsx.insert(tk.END, f"{k + 1}.  {x[k]}")
                 k += 1
-                
+
         # построение графика
         if isxCanvas is not None:
             isxCanvas.get_tk_widget().pack_forget()
-        figIsx = Figure(figsize = (60 , 2))
+        figIsx = Figure(figsize=(60, 2))
         self.ax = figIsx.add_subplot(111)
         self.ax.set_title("Исходный сигнал")
         self.ax.grid(True)
@@ -122,81 +133,102 @@ class Application(tk.Tk):
         self.canvas = FigureCanvasTkAgg(figIsx, master=self.graphIsx)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
-        
+
     def numChannel(self):
         global channelNum, data
         try:
             if data is None:
-                messagebox.showerror('Ошибка!', 'Не был выбран файл')
-                return()
+                messagebox.showerror("Ошибка!", "Не был выбран файл")
+                return ()
         except NameError:
-            messagebox.showerror('Ошибка!', 'Не был выбран файл')
-            return()
-        
-        channelNum = simpledialog.askinteger("Ввод канала", "Введите номер канала (от 1 до 5):")
-        if channelNum is not None and channelNum > 0 and channelNum <= 5 or not channelNum:
+            messagebox.showerror("Ошибка!", "Не был выбран файл")
+            return ()
+
+        channelNum = simpledialog.askinteger(
+            "Ввод канала", "Введите номер канала (от 1 до 5):"
+        )
+        if (
+            channelNum is not None
+            and channelNum > 0
+            and channelNum <= 5
+            or not channelNum
+        ):
             self.channelCF(channelNum)
         else:
-            messagebox.showerror('Ошибка!', 'Введите верное значение канала (целое число от 1 до 5)')
-            return()
-        
+            messagebox.showerror(
+                "Ошибка!",
+                "Введите верное значение канала (целое число от 1 до 5)"
+            )
+            return ()
+
     def spectr(self):
         try:
             if data is None:
-                messagebox.showerror('Ошибка!', 'Не был выбран файл')
-                return()
+                messagebox.showerror("Ошибка!", "Не был выбран файл")
+                return ()
         except NameError:
-            messagebox.showerror('Ошибка!', 'Не был выбран файл')
-            return()
-        X = np.zeros(N, dtype = np.complex64)
+            messagebox.showerror("Ошибка!", "Не был выбран файл")
+            return ()
+        X = np.zeros(N, dtype=np.complex64)
         for k in range(N):
             for n in range(N):
                 X[k] += x[n] * cmath.exp(-1j * 2 * np.pi * k * n / N)
-        
-                    
+
         global spectrCanvas
-        
+
         if spectrCanvas is not None:
             spectrCanvas.get_tk_widget().pack_forget()
         figImpulse = Figure(figsize=(60, 2))
         self.ax = figImpulse.add_subplot(111)
         self.ax.set_title("Спектр")
         self.ax.grid(True)
-        self.ax.bar(np.arange(0, N//2), [cmath.polar(X[k])[0] for k in range(N//2)], color = "black")
+        self.ax.bar(
+            np.arange(0, N // 2),
+            [cmath.polar(X[k])[0] for k in range(N // 2)],
+            color="black",
+        )
         self.canvas = FigureCanvasTkAgg(figImpulse, master=self.graphSpectr)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
         spectrCanvas = self.canvas
-        
+
     def channelCF(self, channelNum):
         if data is None:
-            messagebox.showerror('Ошибка!', 'хуй')
-        count = 1 
+            messagebox.showerror("Ошибка!", "хуй")
+        count = 1
         if channelNum is None:
-            messagebox.showerror('Ошибка!', 'Введите верное значение канала (целое число от 1 до 5)')
-            return()
-        
-    # Вычисляем границы полосы частот для выбранного канала
+            messagebox.showerror(
+                "Ошибка!",
+                "Введите верное значение канала (целое число от 1 до 5)"
+            )
+            return ()
+
+        # Вычисляем границы полосы частот для выбранного канала
         if channelNum is None:
-            messagebox.showerror('Ошибка!', 'Введите верное значение канала (целое число от 1 до 5)')
-            return()
+            messagebox.showerror(
+                "Ошибка!",
+                "Введите верное значение канала (целое число от 1 до 5)"
+            )
+            return ()
         f1 = freqCenter[channelNum - 1] - delta_f / 2
         f2 = freqCenter[channelNum - 1] + delta_f / 2
 
-    # Создаем фильтр для выбранного канала
+        # Создаем фильтр для выбранного канала
         for n in range(M):
             if n == M_2:
                 h[n] = (2 * (f2 - f1)) / freqDiskret
             else:
-                h[n] = (np.sin(2 * cmath.pi * (n - M_2) * f2 / freqDiskret) - np.sin(2 * cmath.pi * (n - M_2) * f1 / freqDiskret)) / (
-                            cmath.pi * (n - M_2))
-    
-     # Фильтруем сигнал с помощью импульсной характеристики
-        y_filtered = np.convolve(x, h, mode='same')
+                h[n] = (
+                    np.sin(2 * cmath.pi * (n - M_2) * f2 / freqDiskret)
+                    - np.sin(2 * cmath.pi * (n - M_2) * f1 / freqDiskret))\
+                        / (cmath.pi * (n - M_2))
+
+        # Фильтруем сигнал с помощью импульсной характеристики
+        y_filtered = np.convolve(x, h, mode="same")
         for value in y_filtered:
             self.listBoxCF.insert(tk.END, f"{count}. {value}")
             count += 1
-        
+
         global cfCanvas
         if cfCanvas is not None:
             cfCanvas.get_tk_widget().pack_forget()
@@ -204,13 +236,13 @@ class Application(tk.Tk):
         self.ax = figCf.add_subplot(111)
         self.ax.set_title("После ЦФ")
         self.ax.grid(True)
-        self.ax.plot(y_filtered, color = "black")
+        self.ax.plot(y_filtered, color="black")
         self.canvas = FigureCanvasTkAgg(figCf, master=self.graphCf)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
         cfCanvas = self.canvas
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = Application()
     app.mainloop()
--- zadanie_3/main.py
+++ zadanie_3/main.py
@@ -7,11 +7,10 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-
 N = 500  # число отсчетов
 M = 65  # порядок фильтра
 M_2 = M // 2
-freqDiskret = 2000 #частота дискретизации
+freqDiskret = 2000  # частота дискретизации
 freqNeiqvist = freqDiskret / 2
 x = np.zeros(N, dtype=np.float32)  # входной сигнал
 y = np.zeros(N, dtype=np.float32)
@@ -24,46 +23,49 @@ class Application(tk.Tk):
         self.title("Задание 3 (ЦОС)")
         self.iconbitmap("KFU.ico")
         self.geometry("1200x700")
-        
-        
+
         self.graphIsx = Frame(self)
         self.graphIsx.pack()
         self.graphIsxEmpty()
-        
+
         self.graphImp = Frame(self)
         self.graphImp.pack()
         self.graphImpEmpty()
-        
+
         self.graphCf = Frame(self)
         self.graphCf.pack()
         self.graphCfEmpty()
-        
 
-        self.file_button = tk.Button(self, text="Выбрать файл и построить\nграфик исходного сигнала", command=self.open_file)
+        self.file_button = tk.Button(
+            self,
+            text="Выбрать файл и построить\nграфик исходного сигнала",
+            command=self.open_file,
+        )
         self.file_button.pack(side="left", anchor="sw", ipadx=10, ipady=2)
-        
+
         self.exit_button = tk.Button(self, text="Выход", command=self.quit)
         self.exit_button.pack(side="right", anchor="se", ipadx=10, ipady=10)
-        
 
-        self.impBtn = Button(self, text="Характеристика h(i)", command=partial(self.numChannel))
+        self.impBtn = Button(
+            self, text="Характеристика h(i)", command=partial(self.numChannel)
+        )
         self.impBtn.pack(side="left", anchor="sw", ipadx=10, ipady=10)
-        
+
         self.cfBtn = Button(self, text="ЦФ", command=self.channelCF)
         self.cfBtn.pack(side="left", anchor="sw", ipadx=10, ipady=10)
-        
+
         self.listBoxIsx = tk.Listbox(self)
         self.listBoxIsx.pack(side="left", anchor="sw", ipadx=5, ipady=2)
-        self.listBoxIsx.insert(tk.END, 'Исходный сигнал')
-        
+        self.listBoxIsx.insert(tk.END, "Исходный сигнал")
+
         self.listBoxImpulse = tk.Listbox(self)
         self.listBoxImpulse.pack(side="left", anchor="sw", ipadx=5, ipady=2)
-        self.listBoxImpulse.insert(tk.END, 'Характеристика h(i)')
-        
+        self.listBoxImpulse.insert(tk.END, "Характеристика h(i)")
+
         self.listBoxCF = tk.Listbox(self)
         self.listBoxCF.pack(side="left", anchor="sw", ipadx=5, ipady=2)
-        self.listBoxCF.insert(tk.END, 'После ЦФ')
-        
+        self.listBoxCF.insert(tk.END, "После ЦФ")
+
     def graphIsxEmpty(self):
         # создание пустого графика
         global isxCanvas
@@ -75,19 +77,20 @@ class Application(tk.Tk):
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
         isxCanvas = self.canvas
-        
+
     def graphImpEmpty(self):
         # создание пустого графика
         global impulseCanvas
         figImpulse = Figure(figsize=(60, 2))
         self.ax = figImpulse.add_subplot(111)
-        self.ax.set_title("Отсчеты импульсной хар-ки h(i). Порядок фильтра M = 65")
+        self.ax.set_title("Отсчеты импульсной хар-ки h(i). "
+                          "Порядок фильтра M = 65")
         self.ax.grid(True)
         self.canvas = FigureCanvasTkAgg(figImpulse, master=self.graphImp)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
-        impulseCanvas = self.canvas 
-        
+        impulseCanvas = self.canvas
+
     def graphCfEmpty(self):
         # создание пустого графика
         global cfCanvas
@@ -98,14 +101,16 @@ class Application(tk.Tk):
         self.canvas = FigureCanvasTkAgg(figCf, master=self.graphCf)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
-        cfCanvas = self.canvas       
-    
+        cfCanvas = self.canvas
+
     def open_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[('.dat файлы', '*.dat')])
+        file_path = filedialog.askopenfilename(filetypes=[(".dat файлы",
+                                                           "*.dat")])
+
         if not file_path:
             messagebox.showerror("Ошибка!", "Файл не был прочитан!")
-            return()
-        with open(file_path, 'rb') as file:
+            return ()
+        with open(file_path, "rb") as file:
             k = 0
             while True:
                 global data
@@ -115,14 +120,14 @@ class Application(tk.Tk):
                     break
                 x[k] = np.frombuffer(data, dtype=np.float32)[0]
                 y[k] = np.frombuffer(data, dtype=np.float32)[0]
-                self.listBoxIsx.insert(tk.END, f'{k + 1}.  {x[k]}')
+                self.listBoxIsx.insert(tk.END, f"{k + 1}.  {x[k]}")
                 k += 1
-                
+
         # построение графика
         global isxCanvas
         if isxCanvas is not None:
             isxCanvas.get_tk_widget().pack_forget()
-        figIsx = Figure(figsize = (60 , 2))
+        figIsx = Figure(figsize=(60, 2))
         self.ax = figIsx.add_subplot(111)
         self.ax.set_title("Исходный сигнал")
         self.ax.grid(True)
@@ -130,97 +135,129 @@ class Application(tk.Tk):
         self.canvas = FigureCanvasTkAgg(figIsx, master=self.graphIsx)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
-        
+
     def numChannel(self):
         global channelNum
         try:
             if data is None:
-                messagebox.showerror('Ошибка!', 'Не был выбран файл')
-                return()
+                messagebox.showerror("Ошибка!", "Не был выбран файл")
+                return ()
         except NameError:
-            messagebox.showerror('Ошибка!', 'Не был выбран файл')
-            return()
-        
-        channelNum = simpledialog.askinteger("Ввод канала", "Введите номер канала (от 1 до 5):")
-        if channelNum is not None and channelNum > 0 and channelNum <= 5 or not channelNum:
+            messagebox.showerror("Ошибка!", "Не был выбран файл")
+            return ()
+
+        channelNum = simpledialog.askinteger(
+            "Ввод канала", "Введите номер канала (от 1 до 5):"
+        )
+        if (
+            channelNum is not None
+            and channelNum > 0
+            and channelNum <= 5
+            or not channelNum
+        ):
             self.impulseChar(channelNum)
-        
+
     def impulseChar(self, channelNum):
         if channelNum is None:
-            messagebox.showerror('Ошибка!', 'Введите верное значение канала (целое число от 1 до 5)')
-            return()
-        freqCenter = channelNum * 40 #несущая частота с полученным каналом
-        freqLow = (freqCenter - 20)
-        freqHigh = (freqCenter + 20)
+            messagebox.showerror(
+                "Ошибка!",
+                "Введите верное значение канала(целое число от 1 до 5)",
+            )
+            return ()
+        freqCenter = channelNum * 40  # несущая частота с полученным каналом
+        freqLow = freqCenter - 20
+        freqHigh = freqCenter + 20
         count = 2
-        
+
         h[0] = (freqHigh - freqLow) / freqNeiqvist
-        self.listBoxImpulse.insert(tk.END, '1. ' + str(h[0]))
-        
+        self.listBoxImpulse.insert(tk.END, "1. " + str(h[0]))
+
         for k in range(-M_2, M_2 + 1):
             if k == 0:
                 h[k + M_2] = (freqHigh - freqLow) / freqNeiqvist
             else:
-                h[k + M_2] = (np.sin(np.pi * k * freqHigh / freqNeiqvist) / (np.pi * k) -
-                            np.sin(np.pi * k * freqLow / freqNeiqvist) / (np.pi * k))
+                h[k + M_2] = np.sin(np.pi * k * freqHigh / freqNeiqvist) / (
+                    np.pi * k
+                ) - np.sin(np.pi * k * freqLow / freqNeiqvist) / (np.pi * k)
                 if k >= 0:
-                    self.listBoxImpulse.insert(tk.END, f"{count}. {str(h[k + M_2])}")
+                    self.listBoxImpulse.insert(
+                        tk.END,
+                        f"{count}. \
+                        {str(h[k + M_2])}",
+                    )
                     count += 1
-                    
+
         global impulseCanvas
         if impulseCanvas is not None:
             impulseCanvas.get_tk_widget().pack_forget()
         figImpulse = Figure(figsize=(60, 2))
         self.ax = figImpulse.add_subplot(111)
-        self.ax.set_title("Отсчеты импульсной хар-ки h(i). Порядок фильтра M = 65")
+        self.ax.set_title(
+            "Отсчеты импульсной хар-ки h(i).\
+            Порядок фильтра M = 65"
+        )
         self.ax.grid(True)
-        self.ax.bar(range(-M_2, M_2 + 1), h, width=0.8, color = "black")
+        self.ax.bar(range(-M_2, M_2 + 1), h, width=0.8, color="black")
         self.canvas = FigureCanvasTkAgg(figImpulse, master=self.graphImp)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
         impulseCanvas = self.canvas
-        
+
     def channelCF(self):
         try:
             if data is None:
-                messagebox.showerror('Ошибка!', 'Не был выбран файл')
-                return()
+                messagebox.showerror("Ошибка!", "Не был выбран файл")
+                return ()
         except NameError:
-            messagebox.showerror('Ошибка!', 'Не был выбран файл')
-            return()
-            
+            messagebox.showerror("Ошибка!", "Не был выбран файл")
+            return ()
+
         global channelNum
-            # Создание цифрового фильтра с прямоугольной идеальной формой АЧХ
+        # Создание цифрового фильтра с прямоугольной идеальной формой АЧХ
         fs = 2000  # Частота дискретизации
         delta_f = 40  # Ширина полосы частот канала
-        f0 = [40, 80, 120, 160, 200]  # Центральные частоты каналов (40, 80, 120, 160, 200 Гц)
+        f0 = [
+            40,
+            80,
+            120,
+            160,
+            200,
+        ]  # Центральные частоты каналов (40, 80, 120, 160, 200 Гц)
         count = 1
-        
-    # Вычисляем границы полосы частот для выбранного канала
+
+        # Вычисляем границы полосы частот для выбранного канала
         try:
             if channelNum is None:
-                messagebox.showerror('Ошибка!', 'Введите верное значение канала (целое число от 1 до 5)')
-                return()
+                messagebox.showerror(
+                    "Ошибка!", "Введите верное значение канала "
+                    "(целое число от 1 до 5)"
+                )
+                return ()
             f1 = f0[channelNum - 1] - delta_f / 2
             f2 = f0[channelNum - 1] + delta_f / 2
         except NameError:
-            messagebox.showerror('Ошибка!', 'Не был выбран канал (можно выбрать через кнопку "Характеристика h(i)")')
-            return()
-            
-    # Создаем фильтр для выбранного канала
+            messagebox.showerror(
+                "Ошибка!",
+                "Не был выбран канал "
+                '(можно выбрать через кнопку "Характеристика h(i)")',
+            )
+            return ()
+
+        # Создаем фильтр для выбранного канала
         for n in range(M):
             if n == M_2:
                 h[n] = (2 * (f2 - f1)) / fs
             else:
-                h[n] = (np.sin(2 * cmath.pi * (n - M_2) * f2 / fs) - np.sin(2 * cmath.pi * (n - M_2) * f1 / fs)) / (
-                            cmath.pi * (n - M_2))
-    
-     # Фильтруем сигнал с помощью импульсной характеристики
-        y_filtered = np.convolve(x, h, mode='same')
+                h[n] = (
+                    np.sin(2 * cmath.pi * (n - M_2) * f2 / fs)
+                    - np.sin(2 * cmath.pi * (n - M_2) * f1 / fs)
+                ) / (cmath.pi * (n - M_2))
+
+        # Фильтруем сигнал с помощью импульсной характеристики
+        y_filtered = np.convolve(x, h, mode="same")
         for value in y_filtered:
             self.listBoxCF.insert(tk.END, f"{count}. {value}")
             count += 1
-        
 
         global cfCanvas
         if cfCanvas is not None:
@@ -229,13 +266,13 @@ class Application(tk.Tk):
         self.ax = figCf.add_subplot(111)
         self.ax.set_title("После ЦФ")
         self.ax.grid(True)
-        self.ax.plot(y_filtered, color = "black")
+        self.ax.plot(y_filtered, color="black")
         self.canvas = FigureCanvasTkAgg(figCf, master=self.graphCf)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(expand=True)
         cfCanvas = self.canvas
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = Application()
     app.mainloop()
